23d16b6dfc9bd0b216660dff63083cff71ffed99
yongjies <yongjies@cadence.com>
2022-05-03 16:58:24 +0800

    Initial Commit

---
+++ virtualDeconstructionTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  virtualDeconstructionTest
+//
+//  Created by 孙永杰 on 2022/5/3.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
